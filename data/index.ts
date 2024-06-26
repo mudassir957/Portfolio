ff76cba96e41ec19f0f0317815f4edbc1cb7d896
Mudassir Shehzad <shahzadmudassir957@gmail.com>
2024-06-26 20:33:20 +0500

    first commit

---
+++ data/index.ts
@@ -0,0 +1,154 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Experience", link: "#experience" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building JS web applications.",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Natours - A Tour Travel Website",
+    des: "A nature tour company to explore tours using HTML, CSS, SASS, JavaScript.",
+    img: "/p1.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg", "/npm.svg", "/sass.svg"],
+    link: "https://naturenow.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Weather - A Weather App",
+    des: "A simple weather app displaying weather forecast information via API.",
+    img: "/p2.png",
+    iconLists: ["/react.svg", "/js.svg", "/html.svg", "/css.svg", "/tail.svg"],
+    link: "https://clearskyapp.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Budgety - A budget Calculator",
+    des: "An application to calculate your monthly income, expenses, profit and Loss.",
+    img: "/p3.png",
+    iconLists: ["/html.svg", "/css.svg", "/sass.svg", "/npm.svg", "/js.svg"],
+    link: "https://mudassir957.github.io/Budgety/",
+  },
+  {
+    id: 4,
+    title: "Dkeeper - A Note Taking App",
+    des: " A simple note taking app built using React and Web3.",
+    img: "/p4.png",
+    iconLists: ["/html.svg", "/css.svg", "/react.svg", "/node.svg", "/js.svg"],
+    link: "https://github.com/mudassir957/Dkeeper",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Android Developer Intern",
+    desc: "Assisted in the development of a android applications using xml and kotlin, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "React Developer",
+    desc: "Designed and developed web application using HTML, CSS, JavaScript, React and Node.js.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Frontend Developer",
+    desc: "A frontend developer crafted latest web applications using React, Next.js and Tailwind CSS.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 3,
+    title: "Learning Web Development",
+    desc: "Learning about latest tools to implement web applications like React, Next.js and Tailwind CSS.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/mudassir957",
+  },
+  {
+    id: 2,
+    img: "/vercel.svg",
+    link: "https://vercel.com/mudassir957s-projects",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/mudassir957/",
+  },
+];
